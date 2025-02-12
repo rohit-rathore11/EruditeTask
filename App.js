d31e3f31dd96c671ec2362a6df26bae24b79a45e
Rohit Rathore <139796333+rohit-rathore11@users.noreply.github.com>
2025-02-11 16:54:57 -0800

    updated

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import { Provider } from 'react-redux';
-import store from './src/redux/store';
-import PokemonManager from './src/components/PokemonManager';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './src/screens/SplashScreen';
+import SignUpScreen from './src/screens/auth/signup/SignUpScreen';
+import LoginScreen from './src/screens/auth/login/Login';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <PokemonManager/>
-    </Provider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen 
+          name="Splash" 
+          component={SplashScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
